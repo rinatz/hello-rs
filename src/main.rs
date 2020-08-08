c483fb5e2f516003583caaecc9fc9323d27862fd
IDA Kenichiro <ida.kenichiro@gmail.com>
2020-08-08 11:12:13 +0900

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,42 @@
+use anyhow::Result;
 use clap_verbosity_flag::Verbosity;
-use exitfailure::ExitFailure;
-use failure::Error;
-use indicatif::{ProgressBar, ProgressStyle};
-use log::info;
+use log::debug;
 use structopt::clap::AppSettings::ColoredHelp;
+use structopt::StructOpt;
 
-pub type Result<T, E = Error> = std::result::Result<T, E>;
-
-#[derive(Debug, paw_structopt::StructOpt, structopt::StructOpt)]
+#[derive(Debug, StructOpt)]
 #[structopt(setting(ColoredHelp))]
 pub struct Args {
     #[structopt(flatten)]
-    verbose: Verbosity,
+    pub verbose: Verbosity,
 
-    /// Help messages here...
-    #[structopt(short = "n", long)]
-    count: u64,
+    /// Output the traditional greeting message "Hello, World!".
+    #[structopt(short, long)]
+    traditional: bool,
+
+    /// Output text instead of the default greeting.
+    #[structopt(short, long, default_value = "Hello, World!")]
+    greeting: String,
 }
 
-pub fn init_logger(verbose: &Verbosity) {
-    if let Some(level) = verbose.log_level() {
+pub fn init(args: &Args) {
+    if let Some(level) = args.verbose.log_level() {
         env_logger::builder()
             .filter_level(level.to_level_filter())
             .init();
     }
 }
 
-pub fn progress(len: u64) -> ProgressBar {
-    let progress = ProgressBar::new(len);
+fn main() -> Result<()> {
+    let args = Args::from_args();
+    init(&args);
 
-    progress.set_style(
-        ProgressStyle::default_bar()
-            .template("{elapsed:>4.cyan} [{bar:40}] {pos:>4}/{len:4} {msg}")
-            .progress_chars("=> "),
-    );
+    debug!("{:?}", args);
 
-    progress
-}
-
-#[paw::main]
-fn main(args: Args) -> Result<(), ExitFailure> {
-    init_logger(&args.verbose);
-
-    info!("Start");
-
-    for _ in progress(args.count).wrap_iter(0..args.count) {
-        std::thread::sleep(std::time::Duration::from_millis(500));
+    if args.traditional {
+        println!("Hello, World!");
+    } else {
+        println!("{}", args.greeting);
     }
 
     Ok(())
